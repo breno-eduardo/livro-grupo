e5ef3e08a3aae7f45e8622ae265a588e6e06cb69
Breno Eduardo <58312601+breno-eduardo@users.noreply.github.com>
2022-12-11 22:29:54 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <a href="http://127.0.0.1:5500/db.json">Area DEV</a>
+    </div>
+  );
+}
+
+export default App;
